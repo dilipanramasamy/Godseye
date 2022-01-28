c4b62221316635b942ef4ecc17ec1db27e84ed39
dilipanramasamy <rams.uchiha09@gmail.com>
2022-01-28 11:59:34 +0530

    initial commit

---
+++ src/org/employee/Employeey.java
@@ -0,0 +1,18 @@
+package org.employee;
+
+public class Employeey {
+	public void one() {
+		System.out.println("worst employee");
+	}
+	public void two() {
+		System.out.println("good employee");		
+	}
+
+	
+	}
+	
+		
+	
+	
+
+
---
+++ src/org/employee/Industries.java
@@ -0,0 +1,22 @@
+package org.employee;
+
+public class Industries  {
+	public void three() {
+		System.out.println("paints industries");
+		
+	}
+	public void four() {
+		System.out.println("auto mobile industries");
+		
+	}
+	public static void main(String[] args) {
+		
+		Industries a = new Industries();
+		
+		a.three();
+		a.four();
+				
+		
+	}
+
+}
